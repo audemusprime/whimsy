f55ae59072a8e0d8901afedd0667531897bb61c6
audemusprime <edbrossa@gmail.com>
2023-02-14 11:39:35 -0500

    completes auth

--- page/views.py
+++ page/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
 # Create your views here.
 
@@ -13,4 +15,15 @@ def about(request):
 
 
 def signup(request):
-    pass
+    error_msg = ""
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("index")
+        else:
+            error_msg = "Invalid credentials - try again"
+    form = UserCreationForm()
+    context = {"form": form, "error": error_msg}
+    return render(request, "registration/signup.html", context)
