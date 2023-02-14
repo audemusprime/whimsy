b8bae3a257a69fb300ef8ee583eef1d65fa44cfc
audemusprime <edbrossa@gmail.com>
2023-02-14 01:14:46 -0500

    adds css framework, fills in index and about

--- page/urls.py
+++ page/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("about/", views.about, name="about"),
-    path("accounts/signup", views.signup, name="signup"),
+    path("accounts/signup/", views.signup, name="signup"),
 ]
