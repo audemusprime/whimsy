ef6fad7d4377366e6746232f6198dc3cace872d4
audemusprime <edbrossa@gmail.com>
2023-02-13 19:44:42 -0500

    expands initial file structure

--- page/views.py
+++ page/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
+
+
+def index(request):
+    return render(request, "index.html")
+
+
+def about(request):
+    return render(request, "about.html")
+
+
+def signup(request):
+    pass
